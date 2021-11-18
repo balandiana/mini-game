c88de7b764e6fd16f3ede3ba3d39827407b72d0d
balandiana <dianabalan508@gmail.com>
2021-11-18 13:49:06 +0200

    mini_game

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const board = document.querySelector('#board')
 const colors = ['#16FF30', '#4CFF30', '#6EFF30', '#90FF30', '#ADFF30', '#CCFF30', '#E9FF30']
- //['#EB519C', '#CC519C', '#B3519C', '#96519C', '#6F519C', '#45519C', '#09519C']['#16FF30', '#4CFF30', '#6EFF30', '#90FF30', '#ADFF30', '#CCFF30', '#E9FF30']
+ //['#EB519C', '#CC519C', '#B3519C', '#96519C', '#6F519C', '#45519C', '#09519C']
 const SQUARES_NUMBER = 600
 
 for (let i = 0; i < SQUARES_NUMBER; i++) {
